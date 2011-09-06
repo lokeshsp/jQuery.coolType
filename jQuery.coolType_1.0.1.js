bbefe6e9e01595f40027c9e46ef0b7f581ca17f3
Alex@codetunnel.com <Alex@codetunnel.com@8b2eb7b5-8f31-4d47-3e61-4edc0a6c9a96>
2011-09-06 14:19:30 +0000

    Added missing semi-colon.

--- jQuery.coolType_1.0.1.js
+++ jQuery.coolType_1.0.1.js
@@ -1,4 +1,4 @@
-﻿//   **************************************************************************
+ //   **************************************************************************
 //   *                                                                        *
 //   *  This program is free software: you can redistribute it and/or modify  *
 //   *  it under the terms of the GNU General Public License as published by  *
@@ -35,7 +35,7 @@
                 inline: false,
                 caretBlinkSpeed: 300,
                 scrollToBottom: false
-            }, options)
+            }, options);
             $(window).data('coolTypeDefaults', defaultSettings);
 
             if (defaultSettings.scrollToBottom && !$.scrollTo)
